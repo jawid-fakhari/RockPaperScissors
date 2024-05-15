cfe0acc203fb2dd5fbf3477379bd1464dd152787
jawid fakhari <jawid_fakhari@live.it>
2024-05-15 23:24:14 +0200

    using Spectre.Console library to remake the game

--- Program.cs
+++ Program.cs
@@ -1,107 +1,189 @@
-﻿
+﻿using Spectre.Console;
 
-string inputPlayer, inputRobot;
+string inputRobot;
 int randNumber;
+
+int playerScore = 0;
+int robotScore = 0;
+
+
 
 bool playAgain = true;
 
-while (playAgain)
+while (true)
 {
-    int playerScore = 0;
-    int robotScore = 0;
 
-    while (playerScore < 3 && robotScore < 3)
+    SelectionPrompt<string> selectionPrompts = new SelectionPrompt<string>()
+               .Title($"Player Score: {playerScore} - Robot Score: {robotScore}")
+               .AddChoices("Rock", "Paper", "Scissors");
+
+    string playerSelection = AnsiConsole.Prompt(selectionPrompts);
+    AnsiConsole.Write($"You choose: {playerSelection} - ");
+
+    Console.Clear();
+
+    Random rand = new Random();
+    randNumber = rand.Next(1, 4);
+
+    switch (randNumber)
     {
-        
-        Console.WriteLine("Choose you object (rock, paper, scissors): ");
-        inputPlayer = Console.ReadLine();
+        case 1:
+            inputRobot = "Rock";
+            Console.WriteLine($"Robot choose: {inputRobot}");
 
-        Random rand = new Random();
-        randNumber = rand.Next(1, 4);
-
-        switch (randNumber)
-        {
-            case 1:
-                inputRobot = "rock";
-                Console.WriteLine($"Robot choose: {inputRobot}");
-                if (inputRobot == inputPlayer)
-                {
-                    Console.WriteLine("Draw! \n\n");
-                }
-                else if (inputPlayer == "paper")
-                {
-                    Console.WriteLine("You won! \n\n");
-                    playerScore++;
-                }
-                else if (inputPlayer == "scissors")
-                {
-                    Console.WriteLine("Robot won! \n\n");
-                    robotScore++;
-                }
-                break;
-            case 2:
-                inputRobot = "paper";
-                Console.WriteLine($"Robot choose: {inputRobot}");
-                if (inputRobot == inputPlayer)
-                {
-                    Console.WriteLine("Draw! \n\n");
-                }
-                else if (inputPlayer == "rock")
-                {
-                    Console.WriteLine("Robot won! \n\n");
-                    robotScore++;
-                }
-                else if (inputPlayer == "scissors")
-                {
-                    Console.WriteLine("You won! \n\n");
-                    playerScore++;
-                }
-                break;
-            case 3:
-                inputRobot = "scissors";
-                Console.WriteLine($"Robot choose: {inputRobot}");
-                if (inputRobot == inputPlayer)
-                {
-                    Console.WriteLine("Draw! \n\n");
-                }
-                else if (inputPlayer == "paper")
-                {
-                    Console.WriteLine("Robot won! \n\n");
-                    robotScore++;
-                }
-                else if (inputPlayer == "rock")
-                {
-                    Console.WriteLine("you won! \n\n");
-                    playerScore++;
-                }
-                break;
-            default:
-                Console.WriteLine("Invalid entry! \n\n");
-                break;
-        }
-
-        if (playerScore == 3)
-        {
-            Console.WriteLine("Player has won");
-        }
-        else if (robotScore == 3)
-        {
-            Console.WriteLine("Robot has won");
-        }
-
-        
-    }
-    Console.WriteLine($"Player score: {playerScore} - Robot score: {robotScore}");
-
-    Console.WriteLine("Do you want paly again?(y/n)");
-    string loop = Console.ReadLine();
-    if (loop == "y")
-    {
-        playAgain = true;
-        Console.Clear();
-    }
-    else if (loop == "n")
-    {
-        playAgain = false;
+            if (inputRobot == playerSelection)
+            {
+                Console.WriteLine("Draw! \n\n");
+            }
+            else if (playerSelection == "Paper")
+            {
+                Console.WriteLine("You won! \n\n");
+                playerScore++;
+            }
+            else if (playerSelection == "Scissors")
+            {
+                Console.WriteLine("Robot won! \n\n");
+                robotScore++;
+            }
+            break;
+        case 2:
+            inputRobot = "Paper";
+            Console.WriteLine($"Robot choose: {inputRobot}");
+            if (inputRobot == playerSelection)
+            {
+                Console.WriteLine("Draw! \n\n");
+            }
+            else if (playerSelection == "Rock")
+            {
+                Console.WriteLine("Robot won! \n\n");
+                robotScore++;
+            }
+            else if (playerSelection == "Scissors")
+            {
+                Console.WriteLine("You won! \n\n");
+                playerScore++;
+            }
+            break;
+        case 3:
+            inputRobot = "Scissors";
+            Console.WriteLine($"Robot choose: {inputRobot}");
+            if (inputRobot == playerSelection)
+            {
+                Console.WriteLine("Draw! \n\n");
+            }
+            else if (playerSelection == "Paper")
+            {
+                Console.WriteLine("Robot won! \n\n");
+                robotScore++;
+            }
+            else if (playerSelection == "Rock")
+            {
+                Console.WriteLine("you won! \n\n");
+                playerScore++;
+            }
+            break;
+        default:
+            Console.WriteLine("Invalid entry! \n\n");
+            break;
     }
 }
+//while (playAgain)
+//{
+//    int playerScore = 0;
+//    int robotScore = 0;
+
+//    while (playerScore < 3 && robotScore < 3)
+//    {
+
+//        Console.WriteLine("Choose you object (Rock, Paper, Scissors): ");
+//        playerSelection = Console.ReadLine();
+
+//        Random rand = new Random();
+//        randNumber = rand.Next(1, 4);
+
+//        switch (randNumber)
+//        {
+//            case 1:
+//                inputRobot = "Rock";
+//                Console.WriteLine($"Robot choose: {inputRobot}");
+//                if (inputRobot == playerSelection)
+//                {
+//                    Console.WriteLine("Draw! \n\n");
+//                }
+//                else if (playerSelection == "Paper")
+//                {
+//                    Console.WriteLine("You won! \n\n");
+//                    playerScore++;
+//                }
+//                else if (playerSelection == "Scissors")
+//                {
+//                    Console.WriteLine("Robot won! \n\n");
+//                    robotScore++;
+//                }
+//                break;
+//            case 2:
+//                inputRobot = "Paper";
+//                Console.WriteLine($"Robot choose: {inputRobot}");
+//                if (inputRobot == playerSelection)
+//                {
+//                    Console.WriteLine("Draw! \n\n");
+//                }
+//                else if (playerSelection == "Rock")
+//                {
+//                    Console.WriteLine("Robot won! \n\n");
+//                    robotScore++;
+//                }
+//                else if (playerSelection == "Scissors")
+//                {
+//                    Console.WriteLine("You won! \n\n");
+//                    playerScore++;
+//                }
+//                break;
+//            case 3:
+//                inputRobot = "Scissors";
+//                Console.WriteLine($"Robot choose: {inputRobot}");
+//                if (inputRobot == playerSelection)
+//                {
+//                    Console.WriteLine("Draw! \n\n");
+//                }
+//                else if (playerSelection == "Paper")
+//                {
+//                    Console.WriteLine("Robot won! \n\n");
+//                    robotScore++;
+//                }
+//                else if (playerSelection == "Rock")
+//                {
+//                    Console.WriteLine("you won! \n\n");
+//                    playerScore++;
+//                }
+//                break;
+//            default:
+//                Console.WriteLine("Invalid entry! \n\n");
+//                break;
+//        }
+
+//        if (playerScore == 3)
+//        {
+//            Console.WriteLine("Player has won");
+//        }
+//        else if (robotScore == 3)
+//        {
+//            Console.WriteLine("Robot has won");
+//        }
+
+
+//    }
+//    Console.WriteLine($"Player score: {playerScore} - Robot score: {robotScore}");
+
+//    Console.WriteLine("Do you want paly again?(y/n)");
+//    string loop = Console.ReadLine();
+//    if (loop == "y")
+//    {
+//        playAgain = true;
+//        Console.Clear();
+//    }
+//    else if (loop == "n")
+//    {
+//        playAgain = false;
+//    }
+//}
